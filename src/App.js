8397b5905c31daef3f10d4e7edf5247fa2da3cb5
BhargaviDharmavaram <bhargavidharmavaram2001@gmail.com>
2024-07-03 15:40:39 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import UploadComponent from './UploadImageComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Upload Images to S3</h1>
+      <UploadComponent />
+    </div>
+  );
+}
+
+export default App;
